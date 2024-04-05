7522276fadbffb5eb9e0a75371f36aaf4976863b
Jesica Oktavia <jesicaoktavia737@gmail.com>
2024-04-05 21:41:24 +0700

    first commit

---
+++ resources/views/user.blade.php
@@ -0,0 +1,25 @@
+<html>
+    <body>
+        <table border="1" cellpadding="2" cellsapcing="0">
+            <tr>
+                <td>ID</td>
+                <td>Username</td>
+                <td>Nama</td>
+                <td>ID Level Pengguna</td>
+                <td>Kode Level</td>
+                <td>Nama Level</td>
+            </tr>
+            @foreach ($data as $d)
+            <tr>
+                <td>{{ $d->user_id }}</td>
+                <td>{{ $d->username }}</td>
+                <td>{{ $d->nama }}</td>
+                <td>{{ $d->level_id }}</td>
+                <td>{{ $d->level->lovel_kode }}</td>
+                <td>{{ $d->level->level_nama }}</td>
+                <td><a href="/user/ubah/{{ $d->user_id }}">Ubah</a> | <a href="/user/hapus/{{ $d->user_id }}">Hapus</a></td>
+            </tr>
+            @endforeach
+        </table>
+    </body>
+</html>
