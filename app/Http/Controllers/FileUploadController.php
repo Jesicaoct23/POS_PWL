1de627e55f9f7e39d1c270c6466cba9950c9e575
Jesica Oktavia <jesicaoktavia737@gmail.com>
2024-05-15 18:17:20 +0700

    validasi file upload_js12

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -7,36 +7,17 @@ use Illuminate\Http\Request;
 class FileUploadController extends Controller
 {
     //
-    public function fileUpload(){
+    public function fileUpload()
+    {
         return view('file-upload');
     }
 
-    public function prosesFileUpload(Request $request){
-       // dump($request->berkas);
-        //return "Pemrosesan file Upload disini";
-        if($request->hasFile('berkas'))
+    public function prosesFileUpload(Request $request)
     {
-        echo"path():".$request->berkas->path();
-        echo"<br>";
-        echo"extension(): ".$request->berkas->extension();
-        echo"<br>";
-        echo"getClientOriginalExtension(): ".
-            $request->berkas->getClientOriginalExtension();
-        echo"<br>";
-        echo"getMimeType(): ".$request->berkas->getMimeType();
-        echo"<br>";
-        echo"getMimeType(): ".$request->berkas->getMimeType();
-        echo"<br>";
-        echo"getClientOrginalNaame():  ".
-             $request->berkas->getClientOriginalName();
-        echo"<br>";
-        echo"getSize(): ".$request->berkas->getSize();
-
-
-    }    
-    else
-    {
-        echo"Tidak ada berkas yang diupload";
-    }
+        // dump($request->berkas);
+        // return "Pemrosesan file Upload disini";
+        $request->validate([
+            'berkas' => 'required|file|image|max:500', ]);
+        echo $request->berkas->getClientOriginalName() . " Lolos validasi";
     }
 }
